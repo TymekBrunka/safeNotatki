54a886003ebd4824c33bb612fc8ada03a8b0f8c3
TymekBrunka <tymkopolo@wp.pl>
2025-06-11 13:28:42 +0200

    init

---
+++ api/src/main.rs
@@ -0,0 +1,60 @@
+use actix_web::HttpResponse;
+use actix_web::Responder;
+use actix_web::{web, App, HttpServer};
+use sqlx::Pool;
+use sqlx::Postgres;
+mod broadcast;
+use self::broadcast::Broadcaster;
+use std::{io, sync::Arc};
+use actix_web_lab::extract::Path;
+
+use sqlx::postgres::PgPoolOptions;
+use sqlx::Row;
+
+pub struct  AppState{
+    broadcaster:Arc<Broadcaster>,
+    db: Pool<Postgres>
+}
+
+// SSE
+pub async fn sse_client(state: web::Data<AppState>) -> impl Responder {
+    state.broadcaster.new_client().await
+}
+
+pub async fn broadcast_msg(
+    state: web::Data<AppState>,
+    Path((msg,)): Path<(String,)>,
+) -> impl Responder {
+    state.broadcaster.broadcast(&msg).await;
+    HttpResponse::Ok().body("msg sent")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:postgres@localhost:5432/safenotatki")
+        .await
+        .expect("Error creating connection pool.");
+
+    let broadcaster = Broadcaster::create();
+
+    sqlx::query("CREATE DATABASE G;")
+        .execute(&pool)
+        .await;
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(AppState {
+                db: pool.clone(),
+                broadcaster: Arc::clone(&broadcaster)
+            }))
+            // This route is used to listen to events/ sse events
+            .route("/events{_:/?}", web::get().to(sse_client))
+            // This route will create a notification
+            .route("/events/{msg}", web::get().to(broadcast_msg))
+    })
+    .bind(format!("{}:{}","127.0.0.1", "8000"))?
+    .run()
+    .await
+}
